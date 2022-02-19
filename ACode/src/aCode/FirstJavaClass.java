89117698ea5c67e723116e2d8741192a32c49d46
Shreyansh Gupta <225shreyansh@gmail.com>
2022-02-19 16:30:47 +0530

    Anuj Bhaiya

---
+++ ACode/src/aCode/FirstJavaClass.java
@@ -0,0 +1,17 @@
+package aCode;
+
+
+//There can only be one public class per java file.
+//Name of the Public class should be same as the 
+//java file or else we get a COMPILE TIME error.
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Shreyansh");
+		
+	}
+
+}
+
