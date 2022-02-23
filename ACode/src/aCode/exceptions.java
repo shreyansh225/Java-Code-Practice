aeff21f679b2f9dcdd0694833c2983ebe6763167
Shreyansh Gupta <225shreyansh@gmail.com>
2022-02-23 09:49:45 +0530

    23/02/2022

--- ACode/src/aCode/exceptions.java
+++ ACode/src/aCode/exceptions.java
@@ -3,8 +3,39 @@ package aCode;
 public class exceptions {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int a[] = new int[3];
+		
+		try {
+			System.out.println(2/0);	
+		}
+		catch(ArrayIndexOutOfBoundsException e)     // will only work for catching the arrayIndex error but not the devide by 0 error 
+		{       
+			System.out.println("Error aaya tha");
+			System.out.println(e.getLocalizedMessage());
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Arithmetic Error aaya tha");
+			System.out.println(e.getLocalizedMessage());
+		 }
+		
+		
+		System.out.println("\n beech ki line \n");
+	    
+		
+		
+		try {
+			System.out.println(2/0);
+			System.out.println(a[4]);	
+		}
+		catch(Exception e)  // Therefore we use just the word Exception to catch any error
+		{       
+			System.out.println("Error aaya tha");
+			System.out.println(e.getLocalizedMessage());
+		}
+		
+		System.out.println("niiche ki line");
 	}
 
 }
