ae5dc9d4ca41b47101364057e8f2f9a810f6e85e
Shreyansh Gupta <225shreyansh@gmail.com>
2022-03-08 21:30:47 +0530

    update

--- ACode/src/aCode/Array.java
+++ ACode/src/aCode/Array.java
@@ -4,14 +4,14 @@ public class Array {
 
 	public static void main(String[] args) {
 		
-//		int marks[] = new int[5];
-//		marks[0] = 23;
-//		marks[1] = 30;
-//		marks[2] = 12;
-//		marks[3] = 20;
-//		marks[4] = 35;
+		int marks[] = new int[5];
+		marks[0] = 23;
+		marks[1] = 30;
+		marks[2] = 12;
+		marks[3] = 20;
+		marks[4] = 35;
 		
-		int marks[] = {23, 12, 56, 34, 99, 45, 69};
+	//	int marks[] = {23, 12, 56, 34, 99, 45, 69};
 		
 		for(int i = 0; i < marks.length ; i++) {
 			System.out.println(marks[i]);
